a29e48804b0978605437925b1609789f0b65a88b
Saurabh <saurabh@lambdatest.com>
2025-01-13 17:55:43 +0530

    Added Java PW Sample

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Run SmartUI Tests With Java Playwright On LambdaTest.
+
+Command to execute
+
+npx smartui exec -- mvn test -D suite=sdk-playwright-java.xml
---
+++ src/test/java/com/lambdatest/SmartUISDKPlaywright.java
@@ -0,0 +1,44 @@
+package com.lambdatest;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import io.github.lambdatest.SmartUISnapshot;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SmartUISDKPlaywright {
+  private Playwright playwright;
+  private Browser browser;
+  private Page page;
+
+  // Setup Playwright and Browser
+  @BeforeMethod
+  public void setup() {
+    playwright = Playwright.create();
+    BrowserType.LaunchOptions launchOptions = new BrowserType.LaunchOptions()
+      .setHeadless(false);
+    browser = playwright.chromium().launch(launchOptions);
+    page = browser.newPage();
+    System.out.println("Playwright Browser initiated");
+  }
+
+  @Test
+  public void basicTest() throws Exception {
+    page.navigate("https://www.lambdatest.com/visual-regression-testing");
+    SmartUISnapshot.smartuiSnapshot(page, "SmartUI");
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    if (browser != null) {
+      browser.close();
+    }
+    if (playwright != null) {
+      playwright.close();
+    }
+  }
+
+}
